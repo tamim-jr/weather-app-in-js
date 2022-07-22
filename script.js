923bf39ee48a51f400e4ff2d1d994fb3736bf363
Tamim Mahmud <internationalinf24@gmail.com>
2022-07-22 23:09:02 +0600

    setup all data in js

--- script.js
+++ script.js
@@ -1,8 +1,55 @@
 let weather = {
-    "apiKey" : "518acb25102a32b26c50a1fee2b7e7bb", 
-    fetchWeather: function(){
-        fetch('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={518acb25102a32b26c50a1fee2b7e7bb}')
-        .then(response => response.json())
-        .then(data => console.log(data));
+    apiKey: "518acb25102a32b26c50a1fee2b7e7bb",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
     },
-};
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "url('https://wallpaperaccess.com/full/5911249.jpg')";
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  weather.fetchWeather("Narail");
+  
